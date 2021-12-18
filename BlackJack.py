b1edf984763b6440f3736c2d89d3784e914e6291
Barnt <brettbasinger@gmail.com>
2021-12-18 02:00:09 -0500

    Updated at 2:00 am Saturday

--- BlackJack.py
+++ BlackJack.py
@@ -1,21 +1,57 @@
-import random
+suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
+ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
+values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8, 'Nine':9, 'Ten':10, 'Jack':10,
+         'Queen':10, 'King':10, 'Ace':11}
 
-class Deck:
-    pass
 
 class Card:
+    def __init__(self, suit, rank):
+        self.suit = suit
+        self.rank = rank
+    def __str__(self):
+        return self.rank + " of " + self.suit
+    def value(self):
+        return values[self.rank]
+
+class Deck:
+    # upon calling for a deck, an array is created using two loops to create multiple cards
+    # then shuffled
+    # added a shuffle method
+    def __init__(self):
+        import random
+        self.deck = []
+        for suit in suits:
+            for rank in ranks:
+                self.deck.append(Card(suit, rank))
+        random.shuffle(self.deck)
     
+    def __str__(self):
+        deck_list = []
+        for card in self.deck:
+            deck_list.append(card)
+        return deck_list
 
+    def shuffle_deck(self):
+        import random
+        random.shuffle(self.deck)
     
+    def deal_card(self):
+        return self.deck.pop()
 
-class Player:
-    pass
+deck = Deck()
+print(deck)
+ace_of_hearts = Card(suits[0], ranks[-1])
+print(ace_of_hearts.value)
 
-class Dealer:
-    pass
 
-class Bots:
-    pass
+# class Player:
+#     pass
 
-class Main:
-    pass
+# class Dealer:
+#     pass
+
+# class Bots:
+#     pass
+
+# class Main:
+#     pass
